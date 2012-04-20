6466b25c3662bc7ecb1e876afa64d6355861624d
Caleb Simpson <the.foner@gmail.com>
2012-04-20 18:33:49 -0400

    Add DChar printing. Add output argument to conversion. Add test cases.

--- string_compression.asm
+++ string_compression.asm
@@ -1,25 +1,31 @@
 ; 4865 6c6c 6f20 776f 726c 6400
 ; h e  l l  o    w o  r l  d NULL
 
+SET Z, 0x8000
 
 SET A, test
-JSR stringCompress
+SET B, 0x1000
+JSR stringToDChar
+SET A, 0x1000
+JSR printDChar
 
 
 :crash SET PC, crash
 
-;void stringCompress(A string* target)
-:stringCompress
+;A dchar* stringToDChar(A string* target, B out dchar* location)
+; Compress a standard string into dchar format
+:stringToDChar
+    SET PUSH, A
     SET PUSH, B
     SET PUSH, X
     SET PUSH, Y
     SET PUSH, Z
 
+    SET Z, B
     SET B, A
-    ADD B, 1
-    SET Z, A
+    ADD B, 1    
     
-    :_loop__stringCompress
+    :_loop__stringToDChar
     SET X, [A]
     SET Y, [B]
     SHL X, 8        ; shift char value to high byte
@@ -27,9 +33,9 @@ JSR stringCompress
     AND Y, 0x00FF   ; clear top byte
     
     IFE X, 0x0000
-      SET PC, _done__stringCompress
+      SET PC, _done__stringToDChar
     IFE Y , 0x0000
-      SET PC, _shortFinish__stringCompress
+      SET PC, _shortFinish__stringToDChar
 
     BOR X, Y        ; combine two halfs
     SET [Z], X      ; save compressed word.
@@ -37,20 +43,58 @@ JSR stringCompress
     ADD A, 2
     ADD B, 2
     ADD Z, 1
-    SET PC, _loop__stringCompress
+    SET PC, _loop__stringToDChar
     
-    :_shortFinish__stringCompress
+    :_shortFinish__stringToDChar
     SET [Z], X
-    SET PC, _return__stringCompress
+    SET PC, _return__stringToDChar
     
-    :_done__stringCompress
+    :_done__stringToDChar
     SET [Z], 0x0000
     
-    :_return__stringCompress
+    :_return__stringToDChar
     SET Z, POP
     SET Y, POP
     SET X, POP
     SET B, POP
+    SET A, POP
     SET PC, POP
     
+;void pringDChar(A dchar* message)
+:printDChar
+    SET PUSH, I
+    SET I, A
+    
+    :_loop__printDChar
+    SET A, [I]          
+    SHR A, 8    
+    IFE A, 0x0000                     ; High character
+    SET PC, _done__printDChar
+    JSR printChar
+    
+    SET A, [I]
+    AND A, 0x00FF
+    IFE A, 0x0000                     ; Low character
+    SET PC, _done__printDChar
+    JSR printChar
+    
+    ADD I, 1
+    SET PC, _loop__printDChar
+
+    :_done__printDChar
+    SET I, POP    
+    SET PC, POP
+    
+    
+;void printChar(A char character)
+; Print a single character to the console.
+:printChar	
+    AND A, 0x00FF
+    BOR A, [VAR_PRINT_COL]
+    SET [Z], A
+    ADD Z, 1    
+    SET PC, POP
+    
+    
 :test DAT "Hello world", 0x0000
+:VAR_PRINT_COL DAT 0xF000
