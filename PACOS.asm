220be14e0b27ab3b13dc6d8925f4cf83ef871af4
Caleb Simpson <the.foner@gmail.com>
2012-04-22 08:11:10 -0400

    Add ability to add sub-directoriest. Also add basic (starts) of cd and ls commands.

--- PACOS.asm
+++ PACOS.asm
@@ -28,7 +28,8 @@ SET PC, PACOS_main_loop
     JSR printLine
 :HLT SET PC, HLT
 
-:root DAT "root", 0x0000
+:root DAT "ROOT", 0x0000
+:bin DAT "BIN", 0x0000
 
 ;void loader(void)
 :loader    
@@ -40,10 +41,26 @@ SET PC, PACOS_main_loop
     SET [_FS_DIR_START], A
     ADD A, 0x2000
     SET [_FS_DAT_START], A
+    SUB [_FS_SP_MAX], 1
     
     SET A, root                 ; CREATE ROOT DIRECTORY AND SET CD
     JSR mkdir
-    SET [CD], A
+    SET [CD], A        
+        
+    SET A, bin
+    JSR mkdir
+    SET J, A
+    SET B, A                    ; ADD BIN DIRECTORY TO ROOT
+    SET A, [CD]
+    JSR addInode
+    
+    SET A, [CD]
+    SET B, J
+    JSR addInode
+    
+    SET A, [CD]
+    SET B, J
+    JSR addInode
     
     SET PC, POP
 
@@ -129,8 +146,32 @@ SET PC, PACOS_main_loop
 	SET PUSH, X
     SET X, A
     
+    ;CASE: "cd"
+    :_next5__runCommand
+    SET A, X
+    SET B, PACOScom_cd
+    SET C, 0x0020
+    JSR compareString
+    IFE A, 0x0000
+    SET PC, _next4__runCommand
+    JSR com_cd
+    SET PC, _done__runCommand
+    
+    ;CASE: "ls"
+    :_next4__runCommand
+    SET A, X
+    SET B, PACOScom_ls
+    SET C, 0x0000
+    JSR compareString
+    IFE A, 0x0000
+    SET PC, _next3__runCommand
+    JSR com_ls
+    SET PC, _done__runCommand
+    
     
     ;CASE: "exit"	
+    :_next3__runCommand
+    SET A, X
     SET B, PACOScom_exit
     SET C, 0x0000
     JSR compareString
@@ -415,6 +456,35 @@ SET PC, PACOS_main_loop
     :_false_compareString
     SET A, 0x0000
     SET PC, POP
+    
+    
+;A bool compareDChar(A dchar* dchar1, B dchar* dchar2)
+; Checks if dchar1 and dchar2 are the same.
+; Returns 0x0001 if they are and 0x0000 if not.
+:compareDChar    
+	:_loop_compareDChar  
+	IFN [A], [B]					; Current character != -> false.
+    SET PC, _false_compareDChar
+    
+    SET C, [A]
+    AND C, 0x00FF                   ;check for xx00 terminator
+    IFE C, 0x0000
+    SET PC, _true_compareDChar
+    IFE [A], 0x0000 				;check for standard 0000 terminator
+    SET PC, _true_compareDChar
+    
+    ADD A, 1						; Use recursion if string is longer.
+    ADD B, 1
+    JSR compareDChar    
+    SET PC, POP
+    
+    :_true_compareDChar
+    SET A, 0x0001    
+    SET PC, POP
+    
+    :_false_compareDChar
+    SET A, 0x0000    
+    SET PC, POP
 
 
     
@@ -424,16 +494,11 @@ SET PC, PACOS_main_loop
     SET PUSH, I
     SET PUSH, X
 
-    SET I, [_FS_DIR_START]
-    :_loop__mkdir
-    SET X, [I]
-    AND X, 0x8000           ; used bit
-    IFE X, 0x0000
-    SET PC, _found__mkdir
-    ADD I, [_FS_DIR_SIZE]
-    SET PC, _loop__mkdir
-
-    :_found__mkdir
+    SET PUSH, A
+    JSR locateFreeInode
+    SET I, A
+    SET A, POP
+    
     SET [I], 0xD000    ;Set status word
     SET B, I
     ADD B, 1
@@ -451,6 +516,101 @@ SET PC, PACOS_main_loop
     
     
     
+;void addInode(A inode* directory, B inode* child)
+; Adds the child inode into the parent directory
+:addInode
+    SET PUSH, X
+    SET PUSH, Y
+    SET PUSH, Z
+    
+    :_check__addInode    
+    SET Z, A
+    SET X, [A]      
+    AND X, 0x4000   ; complete bit
+    IFE X, 0x0000
+    SET PC, _next__addInode
+    SET PC, _found__addInode
+    
+    :_next__addInode
+    SET X, [A]    
+    IFB X, 0x2000   ; following bit
+    SET PC, _following__addInode
+    ADD A, [_FS_DIR_SIZE]
+    SUB A, 1
+    SET A, [A]
+    SET PC, _check__addInode
+    
+    :_following__addInode
+    ADD A, [_FS_DIR_SIZE]
+    SET PC, _check__addInode
+    
+    
+    :_found__addInode
+    SET Y, A
+    ADD Y, [_FS_DIR_SIZE] ; calculate limit
+    SET X, [A]    
+    IFB X, 0x1000   ; start bit
+    ADD A, 6        ; skip over name bits
+    
+    :_search__addInode
+    IFE [A], 0x0000
+    SET PC, _insert__addInode
+    ADD A, 1
+    IFE A, Y
+    SET PC, _full__addInode
+    SET PC, _search__addInode
+    
+    :_insert__addInode
+    SET [A], B
+    SET PC, _done__addInode
+    
+    :_full__addInode    
+    JSR locateFreeInode
+    SET [A], 0xC000         ; setup new status bit
+    AND [Z], 0xB000         ; clear complete bit of previous sector
+    IFE A, Y                ; set following bit of previous sector if needed.
+    SET PC, _setFollowing__addInode
+    ADD Z, [_FS_DIR_SIZE]   ; move Z to point to the last element of previous sector
+    SUB Z, 1
+    ADD A, 1                ; move A to point at first element of new, empty sector
+    SET [A], [Z]            ; move last element from previous sector to be first in new one
+    SET [Z], A              ; change last element of previous sector to be a pointer to this sector.
+    SUB [Z], 1
+    ADD A, 1                ; finally, add the new element
+    SET [A], B
+    SET PC, _done__addInode
+    
+    :_setFollowing__addInode
+    BOR [Z], 0x2000         ; Set following bit of previous sector's status word
+    ADD A, 1                ; insert new pointer into frist element of this sector
+    SET [A], B
+    SET PC, _done__addInode
+    
+    :_done__addInode
+    SET Z, POP
+    SET Y, POP
+    SET X, POP
+    SET PC, POP
+    
+;A inode* locateFreeInode(void)
+; Finds the first available Inode location in memory
+:locateFreeInode    
+    SET PUSH, X
+    
+    SET A, [_FS_DIR_START]
+    :_loop__locateFreeInode
+    SET X, [A]
+    AND X, 0x8000           ; used bit
+    IFE X, 0x0000
+    SET PC, _found__locateFreeInode
+    ADD A, [_FS_DIR_SIZE]
+    SET PC, _loop__locateFreeInode
+    
+    :_found__locateFreeInode
+    SET X, POP
+    SET PC, POP
+    
+    
     
 ;void com_echo(void)
 ; Parses the current command buffer assuming echo command format.
@@ -465,6 +625,97 @@ SET PC, PACOS_main_loop
 	SET A, PACOS_help
     JSR printLine
 	SET PC, POP
+    
+;void ls(void)
+; Displays the inodes in the current directory
+:com_ls
+    SET PUSH, X
+    SET PUSH, Y
+    SET PUSH, C
+    
+    SET X, [CD]
+    SET C, [X]              ;set c to be a copy of the current sector's status word.
+    SET Y, X
+    ADD Y, [_FS_DIR_SIZE] ;set y to the last data element
+    SUB Y, 1
+    
+    IFB C, 0x1000   ;start bit
+    ADD X, 6        ;jump over name
+    
+    :_loop__com_ls
+    IFE [X], 0x0000
+    SET PC, _done__com_ls
+    IFE X, Y
+    SET PC, _last__com_ls
+        
+    SET A, [X]
+    ADD A, 1
+    JSR printDChar
+    JSR newLine    
+    ADD X, 1
+    SET PC, _loop__com_ls
+    
+    :_last__com_ls
+    IFB C, 0x4000   ; complete bit
+    SET PC, _complete__com_ls
+    IFB C, 0x2000   ; following bit
+    SET PC, _following__com_ls
+    
+    SET X, [X]      ; move to next directory sector
+    SET C, [X]      ; C is a copy of the current sector's status word
+    SET Y, X        ; Y points to the last element of the current sector.
+    ADD Y, [_FS_DIR_SIZE]
+    SUB Y, 1
+    ADD X, 1        ; set X to first element
+    SET PC, _loop__com_ls
+        
+    
+    :_complete__com_ls
+    SET A, [X]
+    ADD A, 1
+    JSR printDChar
+    JSR newLine
+    SET PC, _done__com_ls
+    
+    :_following__com_ls
+    ADD Y, [_FS_DIR_SIZE]
+    SET PC, _loop__com_ls
+    
+    :_done__com_ls    
+    
+    SET C, POP
+    SET Y, POP
+    SET X, POP
+    SET PC, POP
+    
+
+;void com_cd(void)
+; parses the current command buffer assuming CD format
+:com_cd
+    SET A, 0xA003   ; Address of arg[0] during CD command
+    SET B, 0xA003
+    JSR stringToDChar
+    SET B, [CD]
+    ADD B, 6
+    SET B, [B]
+    ADD B, 1
+    SET PUSH, B
+    JSR compareDChar
+    IFE A, 0x0000
+    SET PC, _noExist__com_cd
+    
+    SET B, POP
+    SET [CD], B
+    SUB [CD], 1
+    SET PC, POP
+    
+    :_noExist__com_cd
+    SET A, invalid__com_cd
+    JSR printLine
+    SET PC, POP
+:invalid__com_cd DAT "Directory does not exist.", 0x0000
+    
+    
 
 :_.DATA
 
@@ -474,7 +725,7 @@ SET PC, PACOS_main_loop
 			dat "Available Commands:", 0x000A
             dat "help, echo [text], exit", 0x0000
             
-:PACOS_prompt dat 0xFFFF, 0x9000, "$ ", 0x0000
+:PACOS_prompt dat 0xFFFF, 0x9000, ": ", 0x0000
 :PACOS_welcome dat 0xFFFF, 0xA000, "PaC OS Ready.", 0x000A
                dat "Type help for a list of commands", 0x0000
 :PACOS_invalid dat 0xFFFF, 0xC000, "Unknown Command", 0x0000
@@ -485,6 +736,8 @@ SET PC, PACOS_main_loop
 :PACOScom_exit dat "EXIT", 0x0000
 :PACOScom_echo dat "ECHO", 0x0020
 :PACOScom_help dat "HELP", 0x0000
+:PACOScom_ls dat "LS", 0x0000
+:PACOScom_cd dat "CD", 0x0020
 :_.END_COMMANDS dat 0xFFFE
 
 
@@ -496,4 +749,10 @@ SET PC, PACOS_main_loop
     :_FS_DAT_COUNT DAT 512   ; 0x4000 memory space
     :_FS_DIR_START DAT 0
     :_FS_DAT_START DAT 0
+    
+:_.FS_STACK
+    :_FS_SP
+    DAT 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  ; slots for a 20 high stack.
+    :_FS_SP_MAX
+    
 :_.FS
